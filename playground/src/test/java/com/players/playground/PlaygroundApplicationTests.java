6734e93fef6e94b5d26d51bdda0e6d3f99599e68
JAEHONG-PC\jaegh <jaeghong011@gmail.com>
2024-05-29 09:55:31 +0900

    add: springboot app 프로젝트 생성 및 초기 설정
    build.gradle에 의존성 추가
    PlaygroundApplication.java 패기지 변경 및 스캔범위 설정
    application.yaml 프로젝트 전반적인 설정 추가(db/jap설정, 파일 경로 설정)

---
+++ playground/src/test/java/com/players/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.players.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
