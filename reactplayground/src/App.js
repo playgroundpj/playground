f2c187a769d55c33b0b1e8b999457b76c3b00f2c
JAEHONG-PC\jaegh <jaeghong011@gmail.com>
2024-06-03 10:55:03 +0900

    docs: 통합 시 오류난 부분 수정
    통합 시 중복된 부분 수정 및 제거

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -6,15 +6,21 @@ import Main from './pages/Main';
 import Login from './pages/member/Login';
 import Register from './pages/member/Register';
 import Error from './pages/Error';
-<<<<<<< HEAD
 import MemberUpdate from './pages/member/MemberUpdate';
 import FindId from './pages/member/FindId';
 import FindPassword from './pages/member/FindPassword';
 import Notice from './pages/Board_Notice/Notice';
 import MemberManage from './pages/admin/MemberManage';
 import CreateNotice from './pages/Board_Notice/CreateNotice';
-import NoticeDetail from './pages/Board_Notice/NoticeDetail'
-import NoticeModify from './pages/Board_Notice/NoticeModify'
+import NoticeDetail from './pages/Board_Notice/NoticeDetail';
+import NoticeModify from './pages/Board_Notice/NoticeModify';
+import RegisterMenu from './pages/menu/RegisterMenu';
+import Boardgame from './pages/boardgame/BoardGame';
+import Menu from './pages/menu/Menu';
+import SearchGame from './pages/boardgame/SearchGame';
+import SearchMenu from './pages/menu/SearchMenu';
+import RegisterGame from './pages/boardgame/RegisterGame';
+import BoardgameDetail from './pages/boardgame/BoardgameDetail'; 
 
 function App() {
   return (
@@ -38,6 +44,15 @@ function App() {
               
             </Route>
 
+            <Route path="boardgame" element={<Boardgame />} />
+            <Route path="boardgame/register" element={<RegisterGame />} /> 
+            <Route path="boardgame/search" element={<SearchGame />} />
+            <Route path="boardgame/:id" element={<BoardgameDetail />} /> {/* BoardgameDetail 라우트 추가 */}
+            <Route path="menu" element={<Menu />} />
+            <Route path="menu/register" element={<RegisterMenu />} />
+            <Route path="menu/search" element={<SearchMenu />} />
+            <Route path="register-game" element={<RegisterGame />} />
+
           </Route>
 
           <Route path="/login" element={ <Login/> } />
@@ -48,40 +63,7 @@ function App() {
 
         </Routes>      
       </BrowserRouter>
-=======
-import RegisterMenu from './pages/menu/RegisterMenu';
-import Boardgame from './pages/boardgame/BoardGame';
-import Menu from './pages/menu/Menu';
-import SearchGame from './pages/boardgame/SearchGame';
-import SearchMenu from './pages/menu/SearchMenu';
-import RegisterGame from './pages/boardgame/RegisterGame';
-import BoardgameDetail from './pages/boardgame/BoardgameDetail'; 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="mypage" element={<MyPageLayout />}>
-            <Route index element={<Profile />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-          <Route path="boardgame" element={<Boardgame />} />
-          <Route path="boardgame/register" element={<RegisterGame />} /> 
-          <Route path="boardgame/search" element={<SearchGame />} />
-          <Route path="boardgame/:id" element={<BoardgameDetail />} /> {/* BoardgameDetail 라우트 추가 */}
-          <Route path="menu" element={<Menu />} />
-          <Route path="menu/register" element={<RegisterMenu />} />
-          <Route path="menu/search" element={<SearchMenu />} />
-          <Route path="register-game" element={<RegisterGame />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> boardgames/find
   );
 }
 
